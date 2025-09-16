a26555291cd0e45f1bcfe8afc0b6430e150d1dbe
Maysha <shaziahmaysha@gmail.com>
2025-09-16 13:18:29 +0600

    Abstract Class Basics

---
+++ Src/AbsCls/BasicAbs.java
@@ -0,0 +1,25 @@
+package AbsCls;
+//Abstract class
+//Interface class
+//Imagination - Abstraction Idea -
+//Giving child class some restriction thet they must obey !
+//Enforcing child to do something
+//Abstract - Partial Abstraction (Normal + abstraction)
+//Interface - complete Abstraction ( Child Must obey )
+
+
+abstract class animal{
+    //What can be added in abstract Class
+    //Any kind of variable
+    //Constructor
+    //complete method
+    //Abstract method ( A method which has no body ) //Child class must override abstract method
+
+}
+
+
+public class BasicAbs {
+    public static void main(String[] args) {
+
+    }
+}
